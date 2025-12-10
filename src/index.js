34a690751c46b703e129e277b069345532b4ad7c
nishadnp <69951135+nishadnp@users.noreply.github.com>
2025-12-11 01:33:21 +0400

    feat: implement basic tab switch handling

--- src/index.js
+++ src/index.js
@@ -1,4 +1,22 @@
 
 import loadHome from "./home";
+import loadMenu from "./menu";
+import loadAbout from "./about";
 
-loadHome();
+const container = document.getElementById('content');
+
+document.querySelector('nav').addEventListener('click', e => {
+
+    if (e.target.id === 'home-btn') {
+        container.innerHTML = "";
+        loadHome();
+    }
+    else if (e.target.id === 'menu-btn') {
+        container.innerHTML = "";
+        loadMenu();
+    }
+    else if (e.target.id === 'about-btn') {
+        container.innerHTML = "";
+        loadAbout();
+    }
+});
