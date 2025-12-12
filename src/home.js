d98b4e4911679acf4efce1ec9774b6f6e2274c79
nishadnp <69951135+nishadnp@users.noreply.github.com>
2025-12-12 18:45:31 +0400

    feat: add images with alt text in home page

--- src/home.js
+++ src/home.js
@@ -1,4 +1,10 @@
+import image1 from './assets/images/cake-berries.jpg'
+import image2 from './assets/images/fries-burger.jpg'
+import image3 from './assets/images/three-beverages.jpg'
+import image4 from './assets/images/vegetable-salad.jpg'
+
 export default function loadHome() {
+
     const content = document.getElementById('content');
 
     const container = document.createElement('div');
@@ -6,21 +12,37 @@ export default function loadHome() {
 
     const headline = document.createElement('h1');
     headline.textContent = "Urban Bites";
+    container.appendChild(headline);
 
-    const tagline = document.createElement('p');
-    tagline.textContent = "Fresh. Fast. Flavorful."
+    const images = [
+        { src: image1, alt: 'Cake with Berries on plate' },
+        { src: image2, alt: 'Fries and Burger on a plate' },
+        { src: image3, alt: 'Three glasses cups with juice' },
+        { src: image4, alt: 'Vegetable salad in gray bowl' },
+    ];
+    
+    const imagesContainer = document.createElement('div');
+    imagesContainer.classList.add('image-container');
 
-    const image = document.createElement('img');
-    image.src = 'https://via.placeholder.com/300';
+    for (let image of images) {
+        const img = new Image();
+        img.src = image.src;
+        img.alt = image.alt;
+        img.classList.add("food-picture");
+        imagesContainer.appendChild(img);
+    }
+
+    
+
+    container.appendChild(imagesContainer);
+    
 
     const description = document.createElement('p');
     description.textContent = "A vibrant modern café serving quick bites, bold flavors, and fresh ingredients — perfect for your everyday cravings.";
-
-    
-    container.appendChild(headline);
-    container.appendChild(tagline);
     container.appendChild(description);
-    container.appendChild(image);
+    
+    
 
     content.appendChild(container);
 }
+
